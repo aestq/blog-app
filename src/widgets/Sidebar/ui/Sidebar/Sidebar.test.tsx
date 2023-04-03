1be7acd49a8f67dc021a271b208ffbe2182674ff
aestq <aleksan.ermackov@yandex.com>
2023-04-03 18:03:08 +0300

    fix tests for sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
 import { Sidebar } from './Sidebar'
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
+import { renderComponent } from 'shared/lib/tests/renderComponent'
 
 describe('Sidebar', () => {
   test('render', () => {
-    renderWithTranslation(<Sidebar />)
+    renderComponent(<Sidebar />)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   })
 
   test('collapsed sidebar', () => {
-    renderWithTranslation(<Sidebar />)
+    renderComponent(<Sidebar />)
     const sidebar = screen.getByTestId('sidebar')
     const sidebarToggle = screen.getByTestId('sidebar-toggle')
     expect(sidebar).toBeInTheDocument()
