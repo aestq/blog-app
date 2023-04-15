075679d460e71fed1a2f12b4423e04ae0179e3e1
aestq <aleksan.ermackov@yandex.com>
2023-04-15 14:59:15 +0300

    fix unit test

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -19,7 +19,8 @@ export default {
   ],
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
-    '\\.svg': resolve(__dirname, 'jestEmptyComponent.tsx')
+    '\\.svg': resolve(__dirname, 'jestEmptyComponent.tsx'),
+    axios: 'axios/dist/node/axios.cjs'
   },
   moduleFileExtensions: [
     'js',
@@ -37,6 +38,10 @@ export default {
   globals: {
     __IS_DEV__: true
   }
+  // transformIgnorePatterns: [
+  //   '\\\\node_modules\\\\',
+  //   '\\.pnp\\.[^\\\\]+$'
+  // ]
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -187,10 +192,6 @@ export default {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\",
-  //   "\\.pnp\\.[^\\\\]+$"
-  // ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
